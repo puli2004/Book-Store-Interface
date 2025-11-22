cdd3949c21846537efa23a7b0b07c10a58931dac
hportuhondo <portuhondohector@gmail.com>
2025-11-22 11:22:04 -0500

    Add files via upload
    Prototype

---
+++ PrototypeDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BookStoreApp
+{
+    public static class PrototypeDatabase
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book { Id = "12345", Title = "C# Basics", Author = "John Doe" },
+            new Book { Id = "67890", Title = "Programming Logic", Author = "Jane Smith" }
+        };
+    }
+
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
